5eb3ed40fe0c5bfe2955dd16d6c4660ac6258d45
jamaljml18 <jamalmhmmd18@gmail.com>
2023-11-27 14:23:01 +0700

    ke-2

--- pertemuan2/src/pages/login.vue
+++ pertemuan2/src/pages/login.vue
@@ -2,22 +2,40 @@
 import { ref } from 'vue';
 import { useRouter } from 'vue-router';
 import { useAuthStore } from '@/stores/auth';
+import axios from 'axios';
 
 const auth = useAuthStore()
 const username = ref('')
+const password = ref('')
 
 const router = useRouter() 
 
-const onLogin = ()=> {
-    auth.login(username.value)
+const onLogin = async ()=> {
+    // Send a POST request
+    const response = await axios({
+    method: 'post',
+    url: 'http:/localhost:3000/login',
+    data: {
+        username: username.value,
+        password: password.value,
+    }
+    // data: {
+    //     firstName: 'Fred',
+    //     lastName: 'Flintstone'
+    // }
+    });
 
-    router.push('/')
+    console.log(response)
+    // auth.login(username.value)
+
+    // router.push('/')
 }
 </script>
 <template>
     <h1>Login Page</h1>
     <div>
-        <input type="text" v-model="username">
+        <input type="text" v-model="username" placeholder="username">
+        <input type="text" v-model="password" placeholder="password">
         <button @click="onLogin()">Login</button>
     </div>
 </template>
